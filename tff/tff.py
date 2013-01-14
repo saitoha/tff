09fe31a4a20c462ea07c15b41de6edd74611cf8f
saitoha <user@zuse.jp>
2013-01-14 20:24:21 +0900

    Minor fix

--- tff/tff.py
+++ tff/tff.py
@@ -561,10 +561,10 @@ class ParseContext(OutputStream, EventDispatcher):
         if buffering:
             try:
                 from cStringIO import StringIO
-            except:
+            except ImportError:
                 try:
                     from StringIO import StringIO
-                except:
+                except ImportError:
                     from io import StringIO
             self._output = codecs.getwriter(termenc)(StringIO())
         else:
